d3b477fcdb56a18770549b5d12fb6aa719195206
Viktoriia Vorobei <whispa.vi@gmail.com>
2022-02-26 23:35:58 +0300

    add commands hw_terminal_1.txt

---
+++ skript_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd HW_terminal_1
+mkdir myf_1 myf_2 myf_3
+cd myf_1
+touch mtest_1.txt mtest_2.txt mtest_3.txt mj_test1.json mj_test2.json
+mkdir mf_4 mf_5 mf_6
+ls -la
+mv ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_1/mj_test2.json ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_1/mtest_2.txt ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_2

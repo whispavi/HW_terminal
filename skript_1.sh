b6cefff299885d4da0c719dc4bbbb0721f483f19
Tori Vorobei <whispa.vi@gmail.com>
2022-04-16 14:00:11 +0300

    repair 04/16/2022

--- skript_1.sh
+++ skript_1.sh
@@ -5,4 +5,5 @@ cd myf_1
 touch mtest_1.txt mtest_2.txt mtest_3.txt mj_test1.json mj_test2.json
 mkdir mf_4 mf_5 mf_6
 ls -la
-mv ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_1/mj_test2.json ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_1/mtest_2.txt ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_2
+mv mj_test2.json ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_2
+mv mtest_2.txt ~/Desktop/QA/Ksendzov/Terminal/HW_terminal_1/myf_2
